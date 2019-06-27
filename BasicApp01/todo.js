f3dbd965db9fe3e06b98d4bfd3f710b3c1634ea2
sanket2714 <44393387+sanket2714@users.noreply.github.com>
2019-06-27 11:43:55 +0530

    Update todo.js

--- BasicApp01/todo.js
+++ BasicApp01/todo.js
@@ -13,6 +13,37 @@ removeButton.addEventListener('click', removeItem)
 
 function addItem(){
 //console.log('ADD BUTTON CLICKED');
+    //console.log('ADD BUTTON CLICKED');
+    var input=document.getElementById('input')
+    var item=input.value
+    var textnode=document.createTextNode(item)
+
+    if(item === '')
+        return false
+    else{
+        //create li
+        li=document.createElement('li')
+
+        //create checkbox
+        var checkbox=document.createElement('input')
+        checkbox.type='checkbox';
+        checkbox.setAttribute('id','check')
+
+        //create label
+        var label=document.createElement('label')
+
+        //add these elements on web page
+        ul.appendChild(label)
+        li.appendChild(checkbox)
+        label.appendChild(textnode)
+        li.appendChild(label)
+        ul.insertBefore(li,ul.childNodes[0])
+        
+        setTimeout(()=>{
+            li.className='visual';
+        },100)
+        input.value=''
+    }
 
 }
 function removeItem(){
